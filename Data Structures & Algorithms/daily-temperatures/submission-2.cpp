0850a18bcbf31c199539f9a30a5804b54b2bc152
Rahul Prasad <47021075+rahul-siva-prasad@users.noreply.github.com>
2026-07-17 21:31:18 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) 
+    {
+        vector<int> result (temperatures.size(),0);
+        stack<pair<int,int>> waitingForWarm;
+
+        for(int i=0; i< temperatures.size(); i++)
+        {
+
+            while(!waitingForWarm.empty() && (temperatures[i] > waitingForWarm.top().first))
+            {
+                result[waitingForWarm.top().second] = i - waitingForWarm.top().second;
+                waitingForWarm.pop();
+            }
+            waitingForWarm.push({temperatures[i],i});
+        }
+        return result;
+        
+    }
+};
